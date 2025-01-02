4243906c8d7dfa4481c13368d74ad40701bde8df
BK3450D <forffans@gmail.com>
2025-01-02 19:28:53 +0300

    Add files via upload

---
+++ swap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Количество золота: ");
+            int gold = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Количество алмазов: ");
+            int diamond = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"У вас  {gold} золота и {diamond} алмазов");
+            Console.WriteLine("могу обменять на ");
+            (gold, diamond) = (diamond, gold);
+            Console.WriteLine("Золота: " + gold);
+            Console.WriteLine("Злмазов: " + diamond);
+        }
+    }
+}
+
