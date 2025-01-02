5cc57c7d2d2d9119eeab18eab5d03a7a8d7334bb
BK3450D <forffans@gmail.com>
2025-01-02 14:43:39 +0300

    Update Variables.cs

--- Variables.cs
+++ Variables.cs
@@ -7,8 +7,8 @@ namespace ConsoleApp1
     internal class Program
         
     {
-        static void Main(string[] args)
-            
+        
+        static void Main(string[] args) 
         {
             
             bool canSell;
