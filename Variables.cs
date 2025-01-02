568f83877a854ff3ffca3d5aef1868af99ebcdae
BK3450D <forffans@gmail.com>
2025-01-02 14:41:49 +0300

    Update Variables.cs

--- Variables.cs
+++ Variables.cs
@@ -2,36 +2,25 @@
 using System.Text;
 
 namespace ConsoleApp1
+    
 {
     internal class Program
+        
     {
         static void Main(string[] args)
+            
         {
-
+            
             bool canSell;
-
             string login;
-
-            int damege;
-
+            int focus ;
             uint health;
-
             char invite;
-
             byte mineTraffic;
-
             long bigData;
-
             float divide = 1.0f;
-
             double mainDivide = 1.0;
-
-            short checkTime;
-
-
-
-
-
+            short timeChek;
 
         }
     }
