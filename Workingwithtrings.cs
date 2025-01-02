7d574fec1591a3bb6d131f7722afef62b8d6b244
BK3450D <forffans@gmail.com>
2025-01-02 13:12:00 +0300

    Add files via upload

---
+++ Workingwithtrings.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp6 
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+     
+            Console.Write("Ваше имя?: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Сколько вам лет?: ");
+            int age = Convert.ToInt32 (Console.ReadLine());
+
+            
+            Console.Write("Кем вы работаете?:  ");
+            string work = (Console.ReadLine());
+
+            Console.WriteLine($"Вас зовут {name}, вам {age},  и вы работаете {work}.");
+            
+     
+
+        
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
+
+
+
+
