2b8655f64ce804fd07349c8a967ff65f387beb4f
BK3450D <forffans@gmail.com>
2025-01-02 16:51:20 +0300

    Add files via upload

---
+++ fullRows.cs
@@ -0,0 +1,26 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Количество картинок? ");
+            int imagesAmount = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Количество картинок в строке? ");
+            int picturesInRow = Convert.ToInt32(Console.ReadLine());
+
+            int fullRows;
+            int imagesInLastRow;
+
+            fullRows = imagesAmount / picturesInRow;
+            imagesInLastRow = fullRows % picturesInRow;
+
+            Console.WriteLine("Строк картинок: " + fullRows);
+            Console.WriteLine("Картинок не вместилось: " + imagesInLastRow);
+
+        }
+    }
+}
