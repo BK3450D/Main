531c9381ebb615e50506d0a6869456f2d08476b7
BK3450D <forffans@gmail.com>
2025-01-03 18:29:36 +0300

    Add files via upload

---
+++ diamondsShop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            int gold;
+            int diamonds;
+            int diamondUnitPrice = 10;
+
+            Console.WriteLine("Добро пожаловытьв магазин алмазов! 1 алмаз стоит 10 золота");
+            Console.Write("Сколько золота у вас?: ");
+            gold = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько алмазов вы хотите купить?: ");
+            diamonds = Convert.ToInt32(Console.ReadLine());
+            gold -= diamonds * diamondUnitPrice;
+            Console.WriteLine($"Вы купили {diamonds} алмазов.");
+            Console.WriteLine($"У вас осталосб {gold} золота");
+        }
+    }
+}
