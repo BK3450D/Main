d32c03c00364551e35013fa05da52230180332b0
BK3450D <forffans@gmail.com>
2025-01-04 12:50:09 +0300

    Add files via upload

---
+++ clinic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour;
+            int minute;
+            int people;
+            int reception = 10;
+            
+            Console.Write("Введите кол - во пациентов: ");
+            people = Convert.ToInt32(Console.ReadLine());
+            minute = people * reception;
+            hour = minute / 60;
+            minute -= hour * 60;
+            Console.WriteLine($"Вы должны отстоять в очереди {hour} часа и {minute} минут");
+        }
+    }
+}
