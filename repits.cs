ab3ae9dd6e8ccf2e88067dcb2d90d62542ac9863
BK3450D <forffans@gmail.com>
2025-01-06 17:46:20 +0300

    Add files via upload

---
+++ repits.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            int amountRepits;
+            Console.Write("Введите сообщение: ");
+            message = Console.ReadLine();
+            Console.Write("Введите количество повторений:");
+            amountRepits = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < amountRepits; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
