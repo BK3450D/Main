5983d0c3a0988c4c414bc9af6e493494b25f96ab
BK3450D <forffans@gmail.com>
2025-01-06 18:10:51 +0300

    Add files via upload

---
+++ exit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string exit = "exit";
+            string programInput;
+        
+            for (; ; )
+            {
+                Console.Write("Введите exit: ");
+                programInput = Console.ReadLine();
+                if (programInput == exit)
+                {
+                    Console.WriteLine("Конец");
+                break;
+                }
+            } 
+        }
+    }
+}
