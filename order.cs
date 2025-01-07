d0f1160b521837a8f5e03644ecae974b6706b744
BK3450D <forffans@gmail.com>
2025-01-07 17:26:36 +0300

    Add files via upload

---
+++ order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 5;
+            int step = 7;
+            int maxValue = 107;
+
+            for (int order = start; order <= maxValue; order += step)
+            {
+                Console.WriteLine(order);
+            }
+        }
+    }
+}
