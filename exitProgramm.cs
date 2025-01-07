7aa61784651474b414095fa780588efd051c91c5
BK3450D <forffans@gmail.com>
2025-01-07 14:11:13 +0300

    Add files via upload

---
+++ exitProgramm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            string exitCommand = "EXIT";
+
+            Console.Write($"Введите {exitCommand} для завершения программы: ");
+            userInput = Console.ReadLine();
+         
+            while (userInput != exitCommand)
+            {
+                Console.Write("Повторите попытку: ");
+                userInput = Console.ReadLine();
+            }
+            if (userInput == exitCommand)
+            {
+                Console.WriteLine("Программа завершина");
+            }
+        }
+    }
+}
