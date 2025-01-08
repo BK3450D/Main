7e0e9018a954f1b0babc334870a4a00734acf455
BK3450D <forffans@gmail.com>
2025-01-08 16:09:43 +0300

    Add files via upload

---
+++ NUNBER.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int nunber = rand.Next(0, 11);
+            int sumOfNumbers = 0;
+
+            for (int i = 0; i <= nunber; i++)
+            {
+                Console.WriteLine(i);
+
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sumOfNumbers += i;
+                }
+            }
+            Console.WriteLine(sumOfNumbers);
+
+            
+          
+            
+
+     
+        }
+    }
+    
+}
