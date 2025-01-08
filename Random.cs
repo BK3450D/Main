4b6ef956fe37055eae7e2d8a8c1710ad9c224683
BK3450D <forffans@gmail.com>
2025-01-08 18:33:33 +0300

    Add files via upload

---
+++ Random.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace random
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dividerOne = 3;
+            int dividerTwo = 5;
+            int maxRandom = 100;
+            int sumOfNumbers = 0;
+
+            Random random = new Random();
+            int nunber = random.Next(0, maxRandom);
+
+            for (int i = 0; i <= nunber; i++)
+            {
+                Console.WriteLine(i);
+
+                if (i % dividerOne == 0 || i % dividerTwo == 0)
+                {
+                    sumOfNumbers += i;
+                }
+            }
+
+            Console.WriteLine(sumOfNumbers);
+        }
+    }
+}
