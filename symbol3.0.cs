d9346a79ce705b7ecc0c651c18bf9cb7f08038d9
BK3450D <forffans@gmail.com>
2025-01-13 23:33:22 +0300

    Add files via upload

---
+++ symbol3.0.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string userName;
+            string middleLine;
+
+            char userSymbol;
+
+            Console.Write("Введите имя: ");
+            userName = Console.ReadLine();
+
+            Console.Write("Введите символ: ");
+            userSymbol = Convert.ToChar(Console.ReadLine());
+                        
+            middleLine = userSymbol + userName + userSymbol;
+
+            string frame = Convert.ToString(userSymbol);
+
+            for (int frameHeight = 1; frameHeight < middleLine.Length; frameHeight++)
+            {
+                frame += userSymbol;
+            }
+
+            Console.WriteLine(frame);
+            Console.WriteLine(middleLine);
+            Console.WriteLine(frame);
+        }
+    }
+}
