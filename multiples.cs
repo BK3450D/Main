458e0ae432c9e31e8935da1c3e49be676ff31c6c
BK3450D <forffans@gmail.com>
2025-01-19 04:40:01 +0300

    Update multiples.cs

--- multiples.cs
+++ multiples.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace ConsoleApp24
+namespace ConsoleApp25
 {
     internal class Program
     {
