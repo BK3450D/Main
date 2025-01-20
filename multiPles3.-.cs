d50899d48f07636ccde747172fc78990414dd7c1
BK3450D <forffans@gmail.com>
2025-01-20 21:24:00 +0300

    Add files via upload

---
+++ multiPles3.-.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxRandom = 25;
+            int minRandom = 10;
+            int minValue = 50;
+            int maxValue = 150;
+
+            Random random = new Random();
+            int randomNunber = random.Next(minRandom, maxRandom + 1);
+
+            Console.WriteLine($"Сгенерированное число: {randomNunber}");
+
+            int count = 0;
+
+            for (int i = 0; i <= maxValue; i++)
+            {
+                i += randomNunber;
+
+                if (i >= minValue)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Количество чисел от {minValue} до {maxValue}, кратных {randomNunber}: {count}");
+        }
+    }
+}
