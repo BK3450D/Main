c8c52e82858fb41ba30b67573d661469f4fb2717
BK3450D <forffans@gmail.com>
2025-01-20 22:11:01 +0300

    Add files via upload

---
+++ multiPles4.0.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxRandom = 25;
+            int minRandom = 10;
+            int minValue = 50;
+            int maxValue = 150;
+
+            Random random = new Random();
+            int randomNunber = random.Next(minRandom, maxRandom + 1);
+
+            Console.WriteLine($"Сгенерированное число: {randomNunber}");
+
+            int count = 0;
+
+            for (int i = 0; i <= maxValue; i += randomNunber)
+            {
+
+                if (i >= minValue)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Количество чисел от {minValue} до {maxValue}, кратных {randomNunber}: {count}");
+        }
+    }
+    
+}
