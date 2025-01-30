4f24707c18db3c6e54059f546e2e2e6a5a6a957e
BK3450D <forffans@gmail.com>
2025-01-30 15:24:45 +0300

    Add files via upload

---
+++ array.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 10;
+            int cols = 10;
+
+            int minRandom = 1;
+            int maxRandom = 10;
+
+            int[,] array = new int[10, 10];
+
+            Random random = new Random();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(minRandom, maxRandom + 1);
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int lineSum = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                lineSum += array[1, i];
+     
+            }
+
+            int productFirstCols = 1;
+
+            for (int j = 0; j < cols; j++)
+            {
+
+                productFirstCols *= array[j, 0];
+            }
+
+            Console.WriteLine($"Сумма второй строки: {lineSum}");
+            Console.WriteLine($"Произведение первого столбца: {productFirstCols}");
+        }
+    }
+}
