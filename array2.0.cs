874e8aa710ab39dde03fadb28d907df2433bd7f7
BK3450D <forffans@gmail.com>
2025-01-31 20:19:00 +0300

    Update array2.0.cs

--- array2.0.cs
+++ array2.0.cs
@@ -32,7 +32,7 @@ namespace ConsoleApp29
                 }
                 Console.WriteLine();
             }
-
+            
             int lineSum = 0;
 
             for (int i = 0; i < rows; i++)
