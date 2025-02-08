efd82897ec7691baee5af1e706c431a248851a7f
BK3450D <forffans@gmail.com>
2025-02-08 21:25:06 +0100

    Add files via upload

---
+++ MI.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[30];
+
+            int maxRandom = 10;
+            int minRandom = 1;
+
+            Random random = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(minRandom, maxRandom + 1);
+
+                Console.Write(numbers[i] + " ");
+            }
+
+            if (numbers[0] > numbers[1])
+            {
+                Console.WriteLine();
+                Console.WriteLine(numbers[0]);
+            }
+
+            for (int i = 1; i < numbers.Length - 1; i++)
+            {
+                bool isLocalMax = numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1];
+
+                if (isLocalMax == true)
+                {
+                    Console.Write(numbers[i]);
+                    Console.WriteLine();
+                }
+            }
+
+            int lastIndex = numbers.Length - 1;
+           
+            if (numbers[lastIndex] > numbers[lastIndex] - 1)
+            {
+                Console.WriteLine(numbers[lastIndex]);
+            }
+        }
+    }
+}
