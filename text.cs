a6c9742906e70e23f591b288bf9de59392355284
BK3450D <forffans@gmail.com>
2025-02-14 20:05:41 +0100

    Add files via upload

---
+++ text.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lineOfTexy = "Здравствуйте, мне кажется или это слишком простое задание.";
+
+            string[] subs = lineOfTexy.Split(' ');
+
+            foreach (var sub in subs)
+            {
+                Console.WriteLine($" {sub}");
+            }
+        }
+    }
+}
