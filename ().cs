b627e7fecc9be46a2ef8fa88e6be37e083c49456
BK3450D <forffans@gmail.com>
2025-02-15 14:53:35 +0100

    Add files via upload

---
+++ ().cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите ( ) для пороверки: ");
+
+        string input = Console.ReadLine();
+
+        int depth = 0;
+        int maxDepth = 0;
+
+        bool isValid = true;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            char currentSymbol = input[i];
+
+            if (currentSymbol == '(')
+            {
+                depth++;
+
+                if (depth > maxDepth)
+                {
+                    maxDepth = depth;
+                }
+            }
+            else if (currentSymbol == ')')
+            {
+                depth--;
+
+                if (depth < 0)
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+        }
+
+        if (depth != 0)
+        {
+            isValid = false;
+        }
+
+        if (isValid)
+        {
+            Console.WriteLine("Строка является корректным скобочным выражением.");
+            Console.WriteLine($"Максимальная глубина: {maxDepth}");
+        }
+        else
+        {
+            Console.WriteLine("Не является корректным скобочным выражением.");
+        }
+    }
+}
