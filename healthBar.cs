336a2386eb268c0ef5ec3d7f9552aba12953b640
BK3450D <forffans@gmail.com>
2025-02-17 20:41:26 +0100

    Add files via upload

---
+++ healthBar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace healsbar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int health = 4, maxHealth = 10;
+            int mana = 9, maxMana = 10;
+            DrawBars(health, maxHealth, 0, '#');
+            DrawBars(mana, maxMana, 1, '&');
+            
+        }
+        static void DrawBars(int value, int maxValue, int position, char symbol = '|')
+        {
+
+            char startOfHealthBox = '[', endOfHealthBox = ']';
+
+            string bar = "";
+
+            for (int i = 0; i < value; i++)
+            {
+                bar += symbol;
+            }
+
+            Console.SetCursorPosition(0, position);
+            Console.Write(startOfHealthBox);
+            Console.Write(bar);
+
+            bar = "";
+
+            for (int i = value;i < maxValue; i++)
+            {
+                bar += "_";
+            }
+
+            Console.Write(bar + endOfHealthBox);
+        }
+    }
+}
