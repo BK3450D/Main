7dfda613e2bd43b12c7088e2b6b0eb9bbef6c1b8
BK3450D <forffans@gmail.com>
2025-02-17 19:49:12 +0100

    Add files via upload

---
+++ readint2,0.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = GetNumber();
+        Console.WriteLine($"Вы ввели число: {number}");
+    }
+    static void WriteError()
+    {
+        ConsoleColor defautColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Ошибка: Введите число");
+        Console.ForegroundColor = defautColor;
+    }
+    static int GetNumber()
+    {
+        int result = 0;
+        bool isValue = true;
+
+        while (isValue)
+        {
+            Console.Write("Пожалуйста, введите число: ");
+
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out result))
+            {
+                isValue = false;
+            }
+            else
+            {
+                WriteError();
+            }
+        }
+        return result;
+    }
+}
