5c68cd9a151574608a898ea7b1aebfc102a6b564
BK3450D <forffans@gmail.com>
2025-02-21 21:15:55 +0100

    Add files via upload

---
+++ HPbar4.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int barLength = 50;
+
+            int healthProcent = 75;
+
+            int manaProcent = 90;
+
+            int maxPercent = 100;
+
+            int percent = barLength * maxPercent / healthProcent;
+
+            DrawBars(healthProcent, barLength);
+
+            DrawBars(manaProcent, barLength);
+        }
+        static void DrawBars(int percent, int barLength)
+        {
+            int symbolCount = barLength * percent / 100;
+
+            char startOfBoxBars = '[';
+            char endOfBoxBars = ']';
+
+            char fillingBarPercentage = '#';
+            char missingBarPercentage = '_';
+
+            Console.Write(startOfBoxBars);
+
+            string bar = new string(fillingBarPercentage, symbolCount);
+
+            int missingPercentage = barLength - bar.Length;
+
+            string bart =  new string(missingBarPercentage, missingPercentage);
+
+            Console.Write(bar + bart + endOfBoxBars);
+            Console.WriteLine();
+        }
+    }
+}
