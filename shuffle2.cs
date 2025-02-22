16f61b03df4581eb9f6f447365acd8b2101a719a
BK3450D <forffans@gmail.com>
2025-02-22 14:21:10 +0100

    Add files via upload

---
+++ shuffle2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace shafl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArray = new int[10];
+
+            int minRandomNumbers = 0;
+            int maxRandomNumbers = 9;
+
+            Random random = new Random();
+
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                numbersArray[i] = random.Next(minRandomNumbers, maxRandomNumbers);
+            }
+
+            PrintArray(numbersArray);
+
+            Shuffle(numbersArray);
+
+            Console.WriteLine();
+
+            PrintArray(numbersArray);
+        }
+        static void Shuffle(int[] numbersArray)
+        {
+           Random random = new Random();
+
+            int minIndex = 0;
+            int maxIndex = numbersArray.Length - 1;
+
+
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                int randomIndex = random.Next(minIndex, maxIndex);
+
+                int tempNumber = numbersArray[randomIndex];
+
+                numbersArray[randomIndex] = numbersArray[i];
+                numbersArray[i] = tempNumber;
+            }
+        }
+        static void PrintArray(int[] numbers)
+        {
+            foreach (int number in numbers)
+            {
+                Console.Write(number);
+            }
+        }
+    }
+}
+
