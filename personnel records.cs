562a23dc23a1a6508d1f9ed30fca02e7d17ccc19
BK3450D <forffans@gmail.com>
2025-04-01 21:19:57 +0300

    Add files via upload

--- personnel records.cs
+++ personnel records.cs
@@ -1,188 +1,207 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace ConsoleApp14
+namespace ConsoleApp16
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            const string CommandOutDossier = "1"; //Добавить досье
-            const string CommandShowDossier = "2"; // вывести  досье
-            const string CommandDeletDossier = "3"; // удалить досье
-            const string CommandFindByLastName = "4"; // поиск по фамилии
+            const string CommandOutDossier = "1";
+            const string CommandShowDossier = "2";
+            const string CommandDeletDossier = "3";
+            const string CommandFindByLastName = "4";
+            const string CommandexitCommand = "5";
 
-            string exitCommand = "5";
+            string firstName;
+            string lastName;
+            string surName;
+            string position;
+            string initials;
+
+            int userCount = 0;
+
+            bool isRunning = true;
 
             string[] fullName = new string[0];
             string[] jobTitle = new string[0];
 
-            int serialNumber = 1;
-            int index;
-
-
-            bool CanWork = true;
-
-            while (CanWork)
+            while (isRunning)
             {
-                Console.WriteLine("1.Добавить досье.");
-                Console.WriteLine("2.Вывести досье.");
-                Console.WriteLine("3.Удалить досье.");
-                Console.WriteLine("4.Поиск по фамилии.");
-                Console.WriteLine("5.выход.");
-                Console.Write("Ваш выбор: ");
+                Console.WriteLine($"{CommandOutDossier}.Добавить досье.");
+                Console.WriteLine($"{CommandShowDossier}.Вывести досье.");
+                Console.WriteLine($"{CommandDeletDossier}.Удалить досье.");
+                Console.WriteLine($"{CommandFindByLastName}.Поиск по фамилии");
+                Console.WriteLine($"{CommandexitCommand}.выход.");
+
+                Console.Write("\nВаш выбор: ");
 
                 string userInput = Console.ReadLine();
 
                 switch (userInput)
                 {
                     case CommandOutDossier:
-
-                        FilOutDossier(ref fullName, ref jobTitle);
-
+                        RequestData(out firstName, out lastName, out surName, out position, out initials, ref fullName, ref jobTitle);
                         break;
 
                     case CommandShowDossier:
-
-                        ShowDossier(fullName, jobTitle, ref serialNumber);
-
+                        ShowDossier(fullName, jobTitle, userCount, fullName);
                         break;
+
                     case CommandDeletDossier:
-
-                        DeleteDossier(ref fullName, ref jobTitle);
-
+                        RequestDossierRemoval(ref fullName, ref jobTitle, ref userCount);
                         break;
 
                     case CommandFindByLastName:
+                        FindDossier(fullName, jobTitle);
+                        break;
 
-                        FindLastName(fullName);
-
+                    case CommandexitCommand:
+                        CompleteTheProgram(ref isRunning);
                         break;
 
                     default:
-                        Console.WriteLine();
-                        Console.Write("Неверный выбор, попробуйте снова");
-                        Console.WriteLine();
+                        Console.WriteLine("\nНеверный выбор, попробуйте снова\n");
                         break;
                 }
-
-                if (userInput == exitCommand)
-                {
-                    Console.WriteLine("Программа завершина.");
-                    CanWork = false;
-                }
             }
         }
-        static void FilOutDossier(ref string[] fullName, ref string[] jobTitle) // заполнить досье
-        {
-            Console.Write("Введите имя: ");
-            string firstName = Console.ReadLine();
 
-            Console.Write("Введите Фамилию: ");
-            string lastName = Console.ReadLine();
+        static void RequestData(out string firstName, out string lastName, out string surName, out string position, out string initials, ref string[] fullName, ref string[] jobTitle)
+        {
+            Console.Write("\nВведите Фамилию: ");
+            lastName = Console.ReadLine();
+
+            Console.Write("Введите имя: ");
+            firstName = Console.ReadLine();
 
             Console.Write("Введите Отчество: ");
-            string surname = Console.ReadLine();
+            surName = Console.ReadLine();
 
             Console.Write("Введите должность: ");
-            string position = Console.ReadLine();
+            position = Console.ReadLine();
 
-            char symbol = ' ';
+            initials = ($"{lastName} {firstName} {surName}");
 
-            string names = firstName  + symbol + lastName + symbol + surname;
+            RecordingData(ref fullName, initials);
 
-            string[] tempfullName = new string[fullName.Length + 1];
-            string[] tempJobTitle = new string[jobTitle.Length + 1];
+            RecordingData(ref jobTitle, position);
 
-            for (int i = 0; i < fullName.Length; i++)
+            Console.WriteLine("\nДосье добавлено.\n");
+        }
+
+        static void IncreaseArraySize(ref string[] dataArray, string initials)
+        {
+            string[] tempArray = new string[dataArray.Length + 1];
+
+            for (int i = 0; i < dataArray.Length; i++)
             {
-                tempfullName[i] = fullName[i];
-                tempJobTitle[i] = jobTitle[i];
+                tempArray[i] = dataArray[i];
             }
 
-            tempfullName[tempfullName.Length - 1] = names;
-            tempJobTitle[tempJobTitle.Length - 1] = position;
+            tempArray[tempArray.Length - 1] = initials;
 
-            fullName = tempfullName;
-            jobTitle = tempJobTitle;
-
-            Console.WriteLine();
-            Console.WriteLine("Досье добавлено.");
-            Console.WriteLine();
+            dataArray = tempArray;
         }
-        static void ShowDossier(string[] names, string[] jobTitle, ref int index)
+
+        static void ShowDossier(string[] names, string[] jobTitle, int index, string[] fullName)
         {
+            if (fullName.Length == 0)
+            {
+                CheckForValidFile(fullName);
+            }
+
             for (int i = 0; i < names.Length; i++)
             {
-                Console.WriteLine();
-                Console.WriteLine($"{i + index}. {names[i]} - {jobTitle[i]}");
-                Console.WriteLine();
+                Console.WriteLine($"\n{i + index + 1}. {names[i]} - {jobTitle[i]}\n");
             }
         }
-        static void DeleteDossier(ref string[] fullName, ref string[] jobTitle)
+
+        static void CheckForValidFile(string[] fullNames)
         {
-            string[] tempfullUsersNames = new string[fullName.Length - 1];
-            string[] tempJobTitle = new string[jobTitle.Length - 1];
-
-            Console.Write("Введите номер досье для удаление: ");
-
-            int deleteIndex = Convert.ToInt32(Console.ReadLine()) - 1;
-
-            for (int i = 0; i < deleteIndex; i++)
+            if (fullNames.Length == 0)
             {
+                Console.WriteLine("\nДосе не найдено\n");
+            }
+        }
 
-                if (deleteIndex == fullName.Length - 1)
+        static void RequestDossierRemoval(ref string[] fullName, ref string[] position, ref int fileNumber)
+        {
+            if (fullName.Length == 0)
+            {
+                CheckForValidFile(fullName);
+            }
+            else if (fullName.Length > fileNumber)
+            {
+                Console.Write("\nВведите номер досье для удаление:");
+
+                fileNumber = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                ReduceArraySize(ref fullName, fileNumber);
+
+                ReduceArraySize(ref position, fileNumber);
+
+                Console.WriteLine("\nДосье удалено\n");
+            }
+        }
+
+        static void ReduceArraySize(ref string[] dataArray, int fileNumber)
+        {
+            if (dataArray.Length > fileNumber)
+            {
+                string[] templateArray = new string[dataArray.Length - 1];
+
+                for (int i = 0; i < fileNumber; i++)
                 {
-                    Console.WriteLine();
+                    templateArray[i] = dataArray[i];
                 }
 
-                tempfullUsersNames[i] = fullName[i];
-                tempJobTitle[i] = jobTitle[i];
-            }
-
-            for (int i = deleteIndex + 1; i < fullName.Length; i++)
-            {
-                tempfullUsersNames[i - 1] = fullName[i];
-                tempJobTitle[i - 1] = jobTitle[i];
-            }
-
-            fullName = tempfullUsersNames;
-            jobTitle = tempJobTitle;
-
-            Console.WriteLine();
-            Console.WriteLine($"Досбе номер {deleteIndex + 1} удаленно.");
-            Console.WriteLine();
-        }
-        static void FindLastName(string[] fullNames)
-        {
-            string userInput;
-
-            Console.Write("Введите фамлиию для поиска: ");
-
-            userInput = Console.ReadLine();
-
-            bool isFound = false;
-
-            for (int i = 0; i < fullNames.Length; i++)
-            {
-
-
-                string[] splitPartOfName = fullNames[i].Split(' ');
-
-                if (splitPartOfName[0].ToLower() == userInput.ToLower())
+                for (int i = fileNumber + 1; i < dataArray.Length; i++)
                 {
-                    Console.WriteLine($"{i + 1}. {fullNames[i]}");
-                    isFound = true;
+                    templateArray[i - 1] = dataArray[i];
                 }
 
+                dataArray = templateArray;
             }
-
-
-
-
-
         }
 
+        static void FindDossier(string[] fullName, string[] jobTitle)
+        {
+            if (fullName.Length == 0)
+            {
+                CheckForValidFile(fullName);
+            }
+            else if (fullName.Length != 0)
+            {
+                bool found = false;
+
+                Console.Write("Введите фамилию для поискаа: ");
+
+                string userInput = Console.ReadLine();
+
+                for (int i = 0; i < fullName.Length; i++)
+                {
+                    string name = fullName[i];
+
+                    string[] tempDataArray = name.Split(' ');
+
+                    if (tempDataArray[0].ToLower() == userInput.ToLower())
+                    {
+                        Console.WriteLine($"\nНайдено:\n{i + 1}. {fullName[i]} - {jobTitle[i]}\n");
+                        found = true;
+                    }
+                }
+                if (!found)
+                {
+                    Console.WriteLine("\nПо вашему запросу ничего не найдено.\n");
+                }
+            }
+        }
+
+        static void CompleteTheProgram(ref bool isRunning)
+        {
+            Console.Clear();
+            Console.WriteLine("Программа завершина");
+            isRunning = false;
+        }
     }
 }
-
