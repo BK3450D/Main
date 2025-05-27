0bdfc6db73d75ff6a7fd21a69174520a0e637dda
BK3450D <forffans@gmail.com>
2025-05-27 12:51:08 +0300

    Add files via upload

---
+++ Queue.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalAmountMoney = 0;           
+            int numbersInQueue = 0;
+
+            int queueLenght = GenerationQueue();
+
+            Queue<int> purchaseAmount = new Queue<int>(queueLenght);
+
+            GenerationPrice(purchaseAmount, queueLenght);
+
+            while (purchaseAmount.Count > 0)
+            {
+                Console.WriteLine($"Длина очереди: {queueLenght--}");
+                Console.WriteLine($"Количество денег: {totalAmountMoney}");
+
+                totalAmountMoney += purchaseAmount.Peek();
+
+                Console.WriteLine($"Покупатель {++numbersInQueue} купил товар на {purchaseAmount.Dequeue()}$");
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        private static void GenerationPrice(Queue<int> purchaseAmount, int queueLenght)
+        {
+            int mimPrice = 1;
+            int maxPrice = 100;
+
+            Random randPrice = new Random();
+
+            for (int i = 0; i < queueLenght; i++)
+            {
+                int price = randPrice.Next(mimPrice, maxPrice);
+
+                purchaseAmount.Enqueue(price);
+            }
+        }
+
+        private static int GenerationQueue()
+        {
+            int mimLenghtQueue = 3;
+            int maxLenghtQueue = 20;
+
+            Random randQueue = new Random();
+
+            int queue = randQueue.Next(mimLenghtQueue, maxLenghtQueue);
+
+
+            return queue;
+        }
+    }
+}
