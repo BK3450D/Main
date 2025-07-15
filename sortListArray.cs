dbc500bf04b9aeb4701bece3910c25e628945cd4
BK3450D <forffans@gmail.com>
2025-07-15 18:28:17 +0300

    Add files via upload

---
+++ sortListArray.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            int[] arrayNumbers1 = new int[] { 1, 2, 3, 5 };
+            int[] arrayNumbers2 = new int[] { 2, 4, 5, 6 };
+
+            List<int> listNambers = new List<int>();
+
+            GetListArray(listNambers, arrayNumbers1);
+            Console.WriteLine();
+            GetListArray(listNambers, arrayNumbers2);
+
+            listNambers.Sort();
+
+            Console.Write("\nОтсортированный список: ");
+            foreach (var nambers in listNambers)
+            {
+                Console.Write(nambers + " | ");
+            }
+        }
+
+        static void GetListArray(List<int> listNambers, int[] arrayNambers)
+        {
+            Console.Write("Массив: ");
+            GetArrayNumbers(arrayNambers);
+            CombineArrays(listNambers, arrayNambers);
+        }
+
+        static void CombineArrays(List<int> listNambers, int[] arrayNumbers)
+        {
+            foreach (int number in arrayNumbers)
+            {
+                if (listNambers.Contains(number) == false)
+                {
+                    listNambers.Add(number);
+                }
+            }
+        }
+
+        static void GetArrayNumbers(int[] arrayNumbers)
+        {
+            foreach (var item in arrayNumbers)
+            {
+                Console.Write($"{item}  | ");
+            }
+        }
+    }
+}
+
+
