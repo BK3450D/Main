9a75a4569b906d2899986c36d1cdfeba65710935
BK3450D <forffans@gmail.com>
2025-07-18 15:55:26 +0300

    Add files via upload

---
+++ feature.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(3, 3, '#');
+            Renderer renderer = new Renderer();
+      
+            renderer.DrawPlayer(player);
+        }
+    }
+
+    class Player
+    {
+        public char PlayerIcon { get; private set; }
+        public int PositionX { get; private set; }
+        public int PositionY { get; private set; }
+
+        public Player(int positionX, int positionY, char playerIcon)
+        {
+            PositionX = positionX;
+            PositionY = positionY;
+            PlayerIcon = playerIcon;
+        }
+    }
+
+    class Renderer
+    {
+        public void DrawPlayer(Player player)
+        {
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(player.PositionX, player.PositionY);
+            Console.WriteLine(player.PlayerIcon);
+            Console.ReadKey(true);
+        }
+    }
+}
