e0a53ac1c252db22d7b9bba2486d045165f7f52e
BK3450D <forffans@gmail.com>
2025-07-19 11:12:36 +0300

    Update feature.cs

--- feature.cs
+++ feature.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace ConsoleApp6
@@ -16,21 +16,21 @@ namespace ConsoleApp6
 
     class Player
     {
-        public char PlayerIcon { get; private set; }
-        public int PositionX { get; private set; }
-        public int PositionY { get; private set; }
-
-        public Player(int positionX, int positionY, char playerIcon)
+        public Player(int positionX, int positionY, char сharacterIcon)
         {
             PositionX = positionX;
             PositionY = positionY;
-            PlayerIcon = playerIcon;
+            СharacterIcon = playerIcon;
         }
+        public char СharacterIcon { get; private set; }
+        public int PositionX { get; private set; }
+        public int PositionY { get; private set; }
+
     }
 
     class Renderer
     {
-        public void DrawPlayer(Player player)
+        public void DrawInfo(Player player)
         {
             Console.CursorVisible = false;
             Console.SetCursorPosition(player.PositionX, player.PositionY);
