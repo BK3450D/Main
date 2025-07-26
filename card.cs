b3d5610c7256ef0943ba903f71207b143548d015
BK3450D <forffans@gmail.com>
2025-07-26 21:10:43 +0300

    Add files via upload

---
+++ card.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Xml.Linq;
+
+namespace SimpleCardGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minCard = 1;
+            int maxCard = 52;
+
+            Dealer dealer = new Dealer();
+            Player player = new Player();
+
+            Console.WriteLine($"Введите количество карт для раздачи от {minCard} до {maxCard}:");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            dealer.DealCards(player, input);
+
+            player.ShowHand();
+
+            Console.WriteLine("Нажмите любую клавишу для выхода.");
+            Console.ReadKey();
+        }
+    }
+    class Card
+    {
+        public Card(string suit, string rank)
+        {
+            Suit = suit;
+            Rank = rank;
+        }
+
+        public string Suit { get; private set; }
+        public string Rank { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{Rank} {Suit}";
+        }
+    }
+
+    class Deck
+    {
+        private List<Card> _cards;
+
+        private List<string> _suits = new List<string>() { "Hearts", "Diamonds", "Spades", "Clubs" };
+        private List<string> _ranks = new List<string>() { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+
+        public Deck()
+        {
+            CreateDeck();
+            Shuffle();
+        }
+
+        public void CreateDeck()
+        {
+            _cards = new List<Card>();
+
+            foreach (var suit in _suits)
+            {
+                foreach (var rank in _ranks)
+                {
+                    _cards.Add(new Card(suit, rank));
+                }
+            }
+        }
+
+        public Card DealCard()
+        {
+            if (_cards.Count == 0)
+                return null;
+
+            Card card = _cards[0];
+            _cards.RemoveAt(0);
+            return card;
+        }
+
+        public void Shuffle()
+        {
+            Random random = new Random();
+
+            int minRandom = 0;
+            int maxRandom = _cards.Count - 1;
+
+            for (int i = 0; i < _cards.Count; i++)
+            {
+                int randomIndex = random.Next(minRandom, maxRandom + 1);
+
+                Card temp = _cards[i];
+                _cards[i] = _cards[randomIndex];
+                _cards[randomIndex] = temp;
+            }
+        }
+    }
+
+    class Player
+    {
+        public Player()
+        {
+            Hand = new List<Card>();
+        }
+        public List<Card> Hand { get; private set; }
+
+        public void ReceiveCard(Card card)
+        {
+            Hand.Add(card);
+        }
+
+        public void ShowHand()
+        {
+            Console.WriteLine("Ваши карты: ");
+
+            foreach (var card in Hand)
+            {
+                Console.WriteLine(card);
+            }
+        }
+    }
+
+    class Dealer
+    {
+        public Deck Deck { get; private set; }
+
+        public Dealer()
+        {
+            Deck = new Deck();
+        }
+
+        public void DealCards(Player player, int numberCards)
+        {
+            for (int j = 0; j < numberCards; j++)
+            {
+                Card card = Deck.DealCard();
+                player.ReceiveCard(card);
+            }
+        }
+    }
+}
